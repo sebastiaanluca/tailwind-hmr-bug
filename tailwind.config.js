9d5b11a6e4dd8f0ee02611fe4f54f3a130bcc072
Sebastiaan Luca <hello@sebastiaanluca.com>
2021-09-11 15:30:25 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './resources/views/welcome.blade.php',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
